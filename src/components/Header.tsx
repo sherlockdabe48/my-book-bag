0cbb46213da585240fec09229e0b11d03798a247
Poramin Dove <48pigeons@gmail.com>
2026-08-25 17:23:00 +0900

    Hide GLogin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react"
-import { searchBookContext, authContext } from "./App"
+import { searchBookContext } from "./App"
 
 interface HeaderProps {
   inputRef: React.MutableRefObject<(HTMLInputElement | null)[]>
@@ -7,7 +7,6 @@ interface HeaderProps {
 
 export default function Header({ inputRef }: HeaderProps) {
   const { handleGetSearchInputValue } = useContext(searchBookContext)
-  const { user, signIn, signOut, isAuthReady } = useContext(authContext)
   const [inputValue, setInputValue] = useState("")
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -46,33 +45,6 @@ export default function Header({ inputRef }: HeaderProps) {
         </form>
       </div>
 
-      {isAuthReady && (
-        <div className="header__auth">
-          {user ? (
-            <div className="header__user">
-              <span className="header__user-email">{user.email}</span>
-              <button
-                className="header__signout-btn"
-                onClick={signOut}
-                title="Sign out"
-              >
-                Sign out
-              </button>
-            </div>
-          ) : (
-            <button
-              className="header__signin-btn"
-              onClick={signIn}
-              title="Sign in with Google to sync your library"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style={{ marginRight: 6, verticalAlign: "middle" }}>
-                <path d="M21.35 11.1H12v2.8h5.35C16.69 16.13 14.58 17.5 12 17.5c-3.04 0-5.5-2.46-5.5-5.5s2.46-5.5 5.5-5.5c1.39 0 2.65.52 3.61 1.37l2.09-2.09A9.46 9.46 0 0 0 12 3C7.03 3 3 7.03 3 12s4.03 9 9 9c5.24 0 9-3.66 9-8.65 0-.58-.07-1.15-.2-1.7l.55.45z"/>
-              </svg>
-              Sign in
-            </button>
-          )}
-        </div>
-      )}
     </div>
   )
 }
